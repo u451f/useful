5ba4af192204487c3e97db036e9695a011e432b7
Ulrike Uhlig <u@451f.org>
2025-04-07 15:55:10 +0200

    increase quality

--- pdf-to-png.sh
+++ pdf-to-png.sh
@@ -5,7 +5,7 @@ while [ $# -gt 0 ]; do
 pdf=$1
 echo "Converting "$pdf" ..."
 pngfile=`echo "$pdf" | sed 's/\.\w*$/.png/'`
-inkscape "$pdf" -z --export-dpi=90 --export-area-drawing --export-png="$pngfile"
+inkscape "$pdf" -z --export-dpi=100 --export-area-drawing --export-png="$pngfile"
 echo "Converted to "$pngfile""
 shift
 
